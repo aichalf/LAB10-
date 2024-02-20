aa265087e0dc1c61141108b8a9f4dc31db787bcf
aichalf <113554602+aichalf@users.noreply.github.com>
2024-02-19 19:11:02 -0500

    scheme

---
+++ ex2.rkt
@@ -0,0 +1,10 @@
+#lang racket
+;Exercice 2
+; Liste des nombres de 1 à 10
+'(1 2 3 4 5 6 7 8 9 10)
+; Liste des nombres flottants et à virgule fixe
+'(2.5 3.14 6.02 1.618)
+; Liste des fonctions mathématiques élémentaires (+, -, *, /)
+'(+ - * /)
+; Liste des expressions non évaluées
+'(2.0*log(2) 2.0*3^3)
